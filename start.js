507082f88c62edc81f342c8ef28ad514b59482c0
xsvarc <xsvarc@gmail.com>
2025-05-22 19:55:47 +0800

    Update start.js

--- start.js
+++ start.js
@@ -2,19 +2,19 @@
 
 import { WebDAVServer } from "@filen/webdav"
 
-const hostname = "127.0.0.1"
-const port = 1900
+const hostname = "0.0.0.0"
+const port = process.env.PORT || 1900
 const https = false
 const server = new WebDAVServer({
-	hostname,
-	port,
-	https,
-	// Omit the user object
-	authMode: "basic" // Only basic auth is supported in proxy mode
+    hostname,
+    port,
+    https,
+    // Omit the user object
+    authMode: "basic" // Only basic auth is supported in proxy mode
 })
 
 await server.start()
 
 console.log(
-	`WebDAV server started on ${https ? "https" : "http"}://${hostname === "127.0.0.1" ? "local.webdav.filen.io" : hostname}:${port}`
+    `WebDAV server started on ${https ? "https" : "http"}://${hostname === "127.0.0.1" ? "local.webdav.filen.io" : hostname}:${port}`
 )
